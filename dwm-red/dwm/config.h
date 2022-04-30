f53006751017f69b2d4b676955ee27b39df8a449
Hydaelyn <92715832+wamuu-sudo@users.noreply.github.com>
2022-04-30 08:59:45 +0200

    Update config.h

--- dwm-red/dwm/config.h
+++ dwm-red/dwm/config.h
@@ -22,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "キ", "ャ", "ン", "デ", "ィ", "ー", "ロ", "ー", "ズ" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class

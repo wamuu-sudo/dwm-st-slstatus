6841cfa3ff734b4002e8ff57a16b05bf78017585
Hyda <hyda@hyda.fr>
2022-05-15 10:17:59 +0200

    Fix st with cleared blue bg color

--- dwm-millenium-jade/st/config.h
+++ dwm-millenium-jade/st/config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "CaskaydiaCove Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
-float alpha = 0.7;
+float alpha = 0.5;
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
